465cceb2913403d5a3c853b79ae4078bd2095538
AntonHran <anton.hranovskyi@gmail.com>
2023-10-03 00:35:46 +0300

    useless code was removed

--- src/repository/quote.py
+++ src/repository/quote.py
@@ -1,10 +1,8 @@
-from pathlib import Path
-
 from telegram import Update
 from telegram.ext import ContextTypes, MessageHandler, filters
 
 from src.services.constants import QUOTE_TIME
-from src.services.assist_functions import get_json, next_state_new
+from src.services.next_state import next_state_new
 
 
 async def quote_time(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -13,8 +11,6 @@ async def quote_time(update: Update, context: ContextTypes.DEFAULT_TYPE):
         context.user_data["menu_categories"][int(QUOTE_TIME)].append(ans.strip())
     elif ans == "skip":
         context.user_data["menu_categories"][int(QUOTE_TIME)].append("default")
-    lang_dict = await get_json(Path(__file__).parent.parent.joinpath("services/languages.json"))
-    # await update.message.reply_text(text=lang_dict[context.user_data["lang"]]["phr"]["motivation"])
     return await next_state_new(update, context)
 
 
